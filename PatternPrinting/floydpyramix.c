768b9c0698c6a7b4d1a65a77a1712330a9bd7e37
parmarravi7 <118459419+parmarravi7@users.noreply.github.com>
2025-04-13 17:53:20 +0530

    Initial Comit

--- PatternPrinting/floydpyramix.c
+++ PatternPrinting/floydpyramix.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int i, j, n;
     printf("Enter the number: ");
     scanf("%d", &n);
-    int a = 1;
-    for(i = 1; i <= n; i++) {
-        for(j = 1; j <= i; j++) {
-            printf("%d", a);
-            a++;
+    
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", (i+j+1) % 2);
+
+            
         }
         printf("\n");
     }
---
+++ PatternPrinting/starmast.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i,n,space;
+    printf("Enter the number :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(space=1;space<n-i;space++){
+            printf("  ");
+        }
+
+        for(int j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
