e54dfb445dd7a847a5cd75e0b0d39118b06a1728
parmarravi7 <118459419+parmarravi7@users.noreply.github.com>
2025-04-13 14:04:31 +0530

    Initial Commit

---
+++ Loops/nthfibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a=1,b=1,c=0,n;
+    printf("Enter the number of the term : ");
+    scanf("%d\n",&n);
+
+    for(int i=1;i<=n;i++){
+        printf("%d",a);
+        c=a+b;
+        a=b;
+        b=c;
+
+    }
+    printf("\n");
+    return 0;
+}
