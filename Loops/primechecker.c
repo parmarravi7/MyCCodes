a092a134486f5511bac8a117caa8a3ff9e8cf9b3
parmarravi7 <118459419+parmarravi7@users.noreply.github.com>
2025-04-13 12:19:53 +0530

    Initial Commit

--- Loops/primechecker.c
+++ Loops/primechecker.c
@@ -1,20 +1,28 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int n;
     printf("Enter the number : ");
-    scanf("%d",&n);
-    int i;
-    for(i=2;i<=n;i++){
-        if(n%i==0){
-            if(i==n){
-                printf("The number is prime");
-            }else{
-                printf("The number is not prime");
-            }
+    scanf("%d", &n);
+    int i,a=0;
+    for (i = 2; i <= n; i++)
+    {
+        if (n % i == 0)
+        {   a=1;
+            break;
         }
-        
     }
-    
-    return 0;
+    if(n==1){
+        printf("1 is neither a prime nor a composite");
+    }
+    else if (a==1)
+    {
+        printf("The number is prime");
+    }
+    else
+    {
+        printf("The number is coposite");
+    }
 
+    return 0;
 }
